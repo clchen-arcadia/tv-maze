66869da6d54985714d9a114ceb41000dea98eeaf
clchen-arcadia <clchen.arcadia@gmail.com>
2022-10-05 15:34:44 -0400

    Complete until part 4 and got code review

--- tvmaze.js
+++ tvmaze.js
@@ -3,6 +3,7 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const PLACEHOLDER_IMG_URL = "https://tinyurl.com/tv-missing";
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -12,28 +13,55 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+
+async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  console.log('getShowsByTerm is invoked');
+
+  const response = await axios.get("http://api.tvmaze.com/search/shows",
+    { params: {q: term, x: 1} } //?q=${term}&x=1
+  );
+  let outputShows = [];
+  for(let show of response.data){ //great choice for map() here //response.data.show
+    // const show2 = {id: show.id, name: show.name, summary: show.summary,
+    //   image: (show.image === null)? PLACEHOLDER_IMG_URL=> } //ternary
+    let tempObj = {};
+    for(let i of ['id', 'name', 'summary', 'image']){ //'i' is RESERVED for index. use id
+      if(i === 'image'){
+        if(show.show.image === null){
+          tempObj[i] = PLACEHOLDER_IMG_URL;
+        }
+        else{
+          tempObj[i] = show.show.image.medium;
+        }
+      }
+      else{
+        tempObj[i] = show.show[i];
+      }
     }
-  ]
+    outputShows.push(tempObj);
+  }
+
+  return outputShows;
 }
+
+
+// Function no longer needed
+
+// /** Function accepts an array of shows and accesses each one's
+//  *  medium image URL, if it does not exist, it inserts a placeholder.
+//  *  The function then returns the optionally modified array.
+//  */
+// function replaceMissingImgURL(shows){
+//   for(let show of shows){
+//     if(show.show.image === null){
+//       show.show.image = {};
+//       show.show.image.medium = "https://tinyurl.com/tv-missing";
+//     }
+//   }
+//   return shows;
+// }
 
 
 /** Given list of shows, create markup for each and to DOM */
@@ -43,11 +71,11 @@ function populateShows(shows) {
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="${show.image}"
+              alt="${show.name}"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -56,11 +84,12 @@ function populateShows(shows) {
                Episodes
              </button>
            </div>
-         </div>  
+         </div>
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
@@ -69,6 +98,8 @@ function populateShows(shows) {
  */
 
 async function searchForShowAndDisplay() {
+  console.log("searchForShowAndDisplay is invoked");
+
   const term = $("#searchForm-term").val();
   const shows = await getShowsByTerm(term);
 
